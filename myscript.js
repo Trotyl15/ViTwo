3daf5da18584090ad985223b64f6b65779b27943
trotyl <mail@trotyl.xyz>
2024-05-01 20:47:11 -0400

    first commit

---
+++ myscript.js
@@ -0,0 +1,68 @@
+﻿var video;
+$(document).ready(function () {
+  console.log("ready!");
+  console.log($("video")[0]);
+  video = document.getElementsByTagName("video")[0];
+
+  // $("#currentTime")[0].text(video.currentTime);
+  // $("#duration")[0].text(video.duration);
+});
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(request);
+      if( request.message === "start" ) {
+          start();
+      }
+      if(request.state =="play"){
+        video.play();
+      }else if(request.state == "pause"){
+        video.pause();
+      }else if(request.state =="reload"){
+        video.remove();
+        video= document.getElementsByTagName("video")[0];
+        start();
+      }
+  }
+);
+
+function start(){
+  // alert("started");
+  chrome.runtime.sendMessage({ "duration": video.duration });
+}
+// $(document).on("click", function () {
+
+//   console.log($("#currentTime")[0]);
+// (async () => {
+//   const response = await chrome.runtime.sendMessage({ greeting: "hello" });
+//   // do something with response here, not outside the function
+//   console.log(response);
+// })();
+// $("#currentTime").text(video.currentTime);
+// });
+
+
+
+// var iframe = document.querySelector('iframe'), video;
+// if (iframe) {
+//   video = iframe.contentWindow.document.getElementsByTagName('video');
+//   video.play();
+// }
+
+// if (video) {
+//   video.playbackRate = 3
+// }
+// let connection;
+
+// document.getElementById("id_connect").onclick = () => {
+//   connection = new WebSocket("ws://localhost:8080/");
+// };
+
+// document.getElementById("id_send").onclick = () => {
+//   connection.send("hoge");
+// };
+
+// document.getElementById("id_close").onclick = () => {
+//   connection.close();
+// };
---
+++ popup.js
@@ -0,0 +1,50 @@
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, { "message": "start" });
+});
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        console.log(request.duration);
+        if (request.duration >= 3600) {
+            var time = new Date(request.duration * 1000).toISOString().slice(11, 19);
+
+        } else {
+            var time = new Date(request.duration * 1000).toISOString().substring(14, 19)
+        }
+        $("#duration").text(time)
+    }
+);
+
+
+$(function () {
+
+    function play() {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, { "state": "play" });
+        });
+    }
+
+    function pause() {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, { "state": "pause" });
+        });
+    }
+
+    function reload() {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            var activeTab = tabs[0];
+            chrome.tabs.sendMessage(activeTab.id, { "state": "reload" });
+        });
+    }
+    $("#play").on("click", play);
+    $("#pause").on("click", pause);
+    $("#reload").on("click", reload);
+
+
+});
